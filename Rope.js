bf6a01889b33653e1cc692e27c8e270e52aa7e5d
AyaanAquaGamers <72394681+AyaanAquaGamers@users.noreply.github.com>
2021-05-17 16:19:17 +0530

    Add files via upload

--- Rope.js
+++ Rope.js
@@ -1,40 +1,37 @@
-class Rope{
-    constructor(bodyA,pointB){
-        var options={
-            bodyA:bodyA,
-            pointB:pointB,
-            stiffness:0.7,
-            length:250
-        }
+class Rope {
+  constructor(bodyA, pointB) {
+    var options = {
+      bodyA: bodyA,
+      pointB: pointB,
+      stiffness: 1.2,
+      length: 10,
+    };
 
-        this.pointB=pointB
-        this.rope=Constraint.create(options)
-        World.add(world,this.rope)
+    this.pointB = pointB;
+    this.rope = Constraint.create(options);
+    World.add(world, this.rope);
+  }
+  attach(bodyA) {
+    this.rope.bodyA = bodyA;
+  }
+
+  fly() {
+    this.rope.bodyA = null;
+  }
 
 
-    }
+  display() {
+    //if (this.rope.bodyA) {
+      //var pointA = this.rope.bodyA.position;
+      //var pointB = this.pointB;
+      //push();
 
-    attach(body) {
-        this.rope.bodyA = body;
-      }
-    
-      fly() {
-        this.rope.bodyA = null;
-      }
-    
-      display() {
-        if (this.rope.bodyA) {
-          var pointA = this.rope.bodyA.position;
-          var pointB = this.pointB;
-          push();
-    
-          stroke(48, 22, 8);
-          strokeWeight(3);
-    
-          line(pointB.x, pointB.y, pointA.x, pointA.y);
-    
-          pop();
-        }
-      }
-    }
-    
+   //   stroke(48, 22, 8);
+     // strokeWeight(4);
+
+//      line(pointB.x, pointB.y, pointA.x, pointA.y);
+
+  //    pop();
+    //}
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,81 +1,53 @@
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Body=Matter.Body
 const Constraint = Matter.Constraint;
 
-var engine, world;
-var box1, box2, box3,box4,box5,box6,box7,box8,box9,box10,box11,box12,box13,box14,box15,box16,box17,box18,box19,box20, ground;
-var ball, rope
+function preload() {
+  bg = loadImage("gamingbackground2.png");
+}
 
 function setup() {
-  createCanvas(3000, 800);
+  createCanvas(900, 400);
+  
   engine = Engine.create();
   world = engine.world;
 
-  ground = new Ground(600, 600, 1200, 20);
+  ground = new Ground(200, 400, 600, 5);
 
-  box1 = new Box(900, 100, 70, 70);
-  box2 = new Box(900, 100, 70, 70);
-  box3 = new Box(900, 100, 70, 70);
-  box4 = new Box(900, 100, 70, 70);
-  box5 = new Box(900, 100, 70, 70);
-  box6 = new Box(900, 100, 70, 70);
-  box7 = new Box(800, 100, 70, 70);
-  box8 = new Box(800, 100, 70, 70);
-  box9 = new Box(800, 100, 70, 70);
-  box10 = new Box(800, 100, 70, 70);
-  box11 = new Box(800, 100, 70, 70);
-  box12 = new Box(800, 100, 70, 70);
-  box13 = new Box(700, 100, 70, 70);
-  box14 = new Box(700, 100, 70, 70);
-  box15 = new Box(700, 100, 70, 70);
-  box16 = new Box(700, 100, 70, 70);
-  box17 = new Box(700, 100, 70, 70);
-  box18 = new Box(700, 100, 70, 70);
-  box19 = new Box(700, 100, 70, 70);
-  box20 = new Box(700, 100, 70, 70);
+  hero = new Hero(400,3,100,100);
+  rope = new Rope(hero.body, { x: 100, y: 465 });
+  monster = new Monster(380,200,100,100);
 
-  ball = new Ball(200, 200, 50, 50);
-  rope = new Rope(ball.body, { x: 500, y: 50 });
+  box1 = new Box(280,350, 30, 40);
+  box2 = new Box(310, 350, 30, 40);
+  box3 = new Box(340, 350, 30, 40);
+  box4 = new Box(370, 350, 30, 40);
+
+  Engine.run(engine)
 
 }
 
 function draw() {
-  background(180);
-  Engine.update(engine);
+  rectMode(CENTER)
+  background(bg);
   ground.display();
   box1.display();
   box2.display();
   box3.display();
   box4.display()
-  box5.display()
-  box6.display()
-  box7.display()
-  box8.display()
-  box9.display()
-  box10.display()
-  box11.display()
-  box12.display()
-  box13.display()
-  box14.display()
-  box15.display()
-  box16.display()
-  box17.display()
-  box18.display()
-  box19.display()
-  box20.display()
-  
 
-  ball.display();
+  hero.display();
   rope.display();
-
-
+  monster.display();
 
 }
-
-
-function mouseDragged() {
-  Matter.Body.setPosition(ball.body, { x: mouseX, y: mouseY });
+function mouseDragged(){
+  Matter.Body.setPosition(hero.body,{x:mouseX,y:mouseY})
 }
 
-
+function mouseReleased(){
+  rope.fly()
+}
